3a0652245ce6472a494596bbe383c5254dd48199
Flavio <flavio.electricidad@gmail.com>
2025-12-06 11:10:24 -0300

    Estructura inicial: modelos User, servicios, rutas y configuración Express

---
+++ src/models/userModel.js
@@ -0,0 +1,14 @@
+export class User {
+  constructor({ id, email, password, username, role = 'user' }) {
+    this.id = id;
+    this.email = email;
+    this.password = password;
+    this.username = username;
+    this.role = role;
+  }
+
+  toJSON() {
+    const { password, ...userData } = this;
+    return userData;
+  }
+}
